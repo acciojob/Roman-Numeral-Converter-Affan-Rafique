c3d1617099c6b3ffa7bee49714df04f9f6fd38e4
Affan-Rafique <affanrafique9@gmail.com>
2025-09-14 21:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+const express = require("express");
+const bodyParser = require("body-parser");
 
-  //your code here
+const app = express();
+app.use(bodyParser.json());
 
+// Roman numeral converter function
+function toRoman(num) {
+  if (num === 0) return "N"; // Romans used "N" for zero
+
+  const symbols = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ];
+
+  let result = "";
+
+  for (let [roman, value] of symbols) {
+    while (num >= value) {
+      result += roman;
+      num -= value;
+    }
+  }
+
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+// API endpoint
+app.post("/romanConverter", (req, res) => {
+  const { input } = req.body;
 
+  if (typeof input !== "number" || input < 0 || input > 100000) {
+    return res.status(400).json({ error: "Input must be a number between 0 and 100000" });
+  }
 
+  const roman = toRoman(input);
+  res.json({ roman });
+});
 
-
-// do not edit below this line
-module.exports = convertToRoman
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
