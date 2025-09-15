afeea978775c9b1f5b3d4bee55da729f18f79001
Affan-Rafique <affanrafique9@gmail.com>
2025-09-15 16:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-function toRoman(num) {
-      if (num === 0) return "N"; // Romans used "N" (nulla) for zero
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-      const symbols = [
-        ["M", 1000],
-        ["CM", 900],
-        ["D", 500],
-        ["CD", 400],
-        ["C", 100],
-        ["XC", 90],
-        ["L", 50],
-        ["XL", 40],
-        ["X", 10],
-        ["IX", 9],
-        ["V", 5],
-        ["IV", 4],
-        ["I", 1],
-      ];
-
-      let result = "";
-
-      for (let [roman, value] of symbols) {
-        while (num >= value) {
-          result += roman;
-          num -= value;
-        }
-      }
-
-      return result;
+// If in same file, define here
+function convertToRoman(num) {
+  const map = [
+    { value: 1000, numeral: "M" },
+    { value: 900, numeral: "CM" },
+    { value: 500, numeral: "D" },
+    { value: 400, numeral: "CD" },
+    { value: 100, numeral: "C" },
+    { value: 90, numeral: "XC" },
+    { value: 50, numeral: "L" },
+    { value: 40, numeral: "XL" },
+    { value: 10, numeral: "X" },
+    { value: 9, numeral: "IX" },
+    { value: 5, numeral: "V" },
+    { value: 4, numeral: "IV" },
+    { value: 1, numeral: "I" }
+  ];
+  let result = "";
+  for (const { value, numeral } of map) {
+    while (num >= value) {
+      result += numeral;
+      num -= value;
     }
+  }
+  return result;
+}
+
+app.post("/romanConverter", (req, res) => {
+  const input = req.body.input;
+  const roman = convertToRoman(input);
+  res.json({ roman });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+
 
    
