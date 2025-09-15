0b7aa1b0dd68a9666b5a6de5dc8d3cdf203b0214
Affan-Rafique <affanrafique9@gmail.com>
2025-09-15 16:05:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const app = express();
-app.use(bodyParser.json());
-
-// Roman numeral converter function
 function toRoman(num) {
-  if (num === 0) return "N"; // Romans used "N" for zero
+      if (num === 0) return "N"; // Romans used "N" (nulla) for zero
 
-  const symbols = [
-    ["M", 1000],
-    ["CM", 900],
-    ["D", 500],
-    ["CD", 400],
-    ["C", 100],
-    ["XC", 90],
-    ["L", 50],
-    ["XL", 40],
-    ["X", 10],
-    ["IX", 9],
-    ["V", 5],
-    ["IV", 4],
-    ["I", 1],
-  ];
+      const symbols = [
+        ["M", 1000],
+        ["CM", 900],
+        ["D", 500],
+        ["CD", 400],
+        ["C", 100],
+        ["XC", 90],
+        ["L", 50],
+        ["XL", 40],
+        ["X", 10],
+        ["IX", 9],
+        ["V", 5],
+        ["IV", 4],
+        ["I", 1],
+      ];
 
-  let result = "";
+      let result = "";
 
-  for (let [roman, value] of symbols) {
-    while (num >= value) {
-      result += roman;
-      num -= value;
+      for (let [roman, value] of symbols) {
+        while (num >= value) {
+          result += roman;
+          num -= value;
+        }
+      }
+
+      return result;
     }
-  }
 
-  return result;
-}
-
-// API endpoint
-app.post("/romanConverter", (req, res) => {
-  const { input } = req.body;
-
-  if (typeof input !== "number" || input < 0 || input > 100000) {
-    return res.status(400).json({ error: "Input must be a number between 0 and 100000" });
-  }
-
-  const roman = toRoman(input);
-  res.json({ roman });
-});
-
-// Start server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+   
